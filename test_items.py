1234a2262d38c9173cba8946bfbdd67d5464d3de
SergeyPetryakov <124886793+zeerrrg@users.noreply.github.com>
2023-11-02 11:47:46 +0300

    Update test_items.py
    Corrected XPATH

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_button_add_to_basket(browser):
     browser.get(link)
-    res = browser.find_elements(By.XPATH, '//*[@id="add_to_basket_form"]/bu')
+    res = browser.find_elements(By.XPATH, '//*[@id="add_to_basket_form"]/button')
     assert len(res) > 0, "Button not found!"
